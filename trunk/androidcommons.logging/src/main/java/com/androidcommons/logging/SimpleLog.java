4751441cc3623365bae30263ecc72c5eed528ed3
denismigol <denismigol@46493231-44d8-eb8f-c521-964da5696728>
2011-04-17 18:56:40 +0000

    full SimpleLog

--- trunk/androidcommons.logging/src/main/java/com/androidcommons/logging/SimpleLog.java
+++ trunk/androidcommons.logging/src/main/java/com/androidcommons/logging/SimpleLog.java
@@ -23,11 +23,43 @@ public class SimpleLog {
 		this.tag = tag;
 	}
 
+	public void verbose(final String message) {
+		Log.println(Log.VERBOSE, tag, message);
+	}
+
+	public void verbose(final String message, final Throwable tr) {
+		Log.println(Log.VERBOSE, tag, message + '\n' + Log.getStackTraceString(tr));
+	}
+
+	public void debug(final String message) {
+		Log.println(Log.DEBUG, tag, message);
+	}
+
+	public void debug(final String message, final Throwable tr) {
+		Log.println(Log.DEBUG, tag, message + '\n' + Log.getStackTraceString(tr));
+	}
+
 	public void info(final String message) {
 		Log.println(Log.INFO, tag, message);
 	}
 
 	public void info(final String message, final Throwable tr) {
 		Log.println(Log.INFO, tag, message + '\n' + Log.getStackTraceString(tr));
+	}
+
+	public void warn(final String message) {
+		Log.println(Log.WARN, tag, message);
+	}
+
+	public void warn(final String message, final Throwable tr) {
+		Log.println(Log.WARN, tag, message + '\n' + Log.getStackTraceString(tr));
+	}
+
+	public void error(final String message) {
+		Log.println(Log.ERROR, tag, message);
+	}
+
+	public void error(final String message, final Throwable tr) {
+		Log.println(Log.ERROR, tag, message + '\n' + Log.getStackTraceString(tr));
 	}
 }
