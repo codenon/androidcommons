3baaf9f34ea04bb2d240369c46399ea97e6a4ce9
denismigol <denismigol@46493231-44d8-eb8f-c521-964da5696728>
2011-08-14 17:49:58 +0000

    init BitmapUtil class

---
+++ util/src/main/java/com/androidcommons/util/BitmapUtil.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) 2011 Android Commons
+ * 
+ * http://www.androidcommons.com/
+ */
+package com.androidcommons.util;
+
+/**
+ * @author Denis Migol
+ * 
+ */
+public class BitmapUtil {
+	private BitmapUtil() {
+	}
+}
