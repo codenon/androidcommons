84032c5ddbc5d743c897862fdac7e5929902374c
denismigol <denismigol@46493231-44d8-eb8f-c521-964da5696728>
2011-08-15 01:22:27 +0000

    code cleanup

--- demos/AlertDialogDemo/src/main/java/com/androidcommons/demos/alertdialog/AlertDialogDemoActivity.java
+++ demos/AlertDialogDemo/src/main/java/com/androidcommons/demos/alertdialog/AlertDialogDemoActivity.java
@@ -5,6 +5,7 @@ import java.util.LinkedHashMap;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -23,67 +24,71 @@ public class AlertDialogDemoActivity extends Activity {
 	private LinkedHashMap<String, Runnable> createTitleDialogMap() {
 		final LinkedHashMap<String, Runnable> ret = new LinkedHashMap<String, Runnable>();
 		final Context context = this;
+		final Resources res = getResources();
 
-		ret.put("OK dialog", new Runnable() {
+		final String message = res.getString(R.string.dialog_message);
+
+		final String okDialog = res.getString(R.string.ok_dialog);
+		ret.put(okDialog, new Runnable() {
 
 			@Override
 			public void run() {
-				AlertDialogUtil.showOkDialog(AlertDialogUtil.alertBuilder(context), R.string.dialog_title,
-						R.string.dialog_message);
+				AlertDialogUtil.showOkDialog(AlertDialogUtil.alertBuilder(context), okDialog, message);
 			}
 		});
 
-		ret.put("OK/Cancel dialog", new Runnable() {
+		final String okCancelDialog = res.getString(R.string.ok_cancel_dialog);
+		ret.put(okCancelDialog, new Runnable() {
 
 			@Override
 			public void run() {
-				AlertDialogUtil.showOkCancelDialog(AlertDialogUtil.alertBuilder(context), R.string.dialog_title,
-						R.string.dialog_message, null, null);
+				AlertDialogUtil.showOkCancelDialog(AlertDialogUtil.alertBuilder(context), okCancelDialog, message,
+						null, null);
 			}
 		});
 
-		ret.put("Yes/No dialog", new Runnable() {
+		final String yesNoDialog = res.getString(R.string.yes_no_dialog);
+		ret.put(yesNoDialog, new Runnable() {
 
 			@Override
 			public void run() {
-				AlertDialogUtil.showYesNoDialog(AlertDialogUtil.alertBuilder(context), R.string.dialog_title,
-						R.string.dialog_message, null, null);
+				AlertDialogUtil.showYesNoDialog(AlertDialogUtil.alertBuilder(context), yesNoDialog, message, null, null);
 			}
 		});
 
-		ret.put("Dialer dialog", new Runnable() {
+		final String dialerDialog = res.getString(R.string.dialer_dialog);
+		ret.put(dialerDialog, new Runnable() {
 
 			@Override
 			public void run() {
-				AlertDialogUtil.showOkDialog(AlertDialogUtil.dialerBuilder(context), R.string.dialog_title,
-						R.string.dialog_message);
+				AlertDialogUtil.showOkDialog(AlertDialogUtil.dialerBuilder(context), dialerDialog, message);
 			}
 		});
 
-		ret.put("Email dialog", new Runnable() {
+		final String emailDialog = res.getString(R.string.email_dialog);
+		ret.put(emailDialog, new Runnable() {
 
 			@Override
 			public void run() {
-				AlertDialogUtil.showOkDialog(AlertDialogUtil.emailBuilder(context), R.string.dialog_title,
-						R.string.dialog_message);
+				AlertDialogUtil.showOkDialog(AlertDialogUtil.emailBuilder(context), emailDialog, message);
 			}
 		});
 
-		ret.put("Info dialog", new Runnable() {
+		final String infoDialog = res.getString(R.string.info_dialog);
+		ret.put(infoDialog, new Runnable() {
 
 			@Override
 			public void run() {
-				AlertDialogUtil.showOkDialog(AlertDialogUtil.infoBuilder(context), R.string.dialog_title,
-						R.string.dialog_message);
+				AlertDialogUtil.showOkDialog(AlertDialogUtil.infoBuilder(context), infoDialog, message);
 			}
 		});
 
-		ret.put("Map dialog", new Runnable() {
+		final String mapDialog = res.getString(R.string.map_dialog);
+		ret.put(mapDialog, new Runnable() {
 
 			@Override
 			public void run() {
-				AlertDialogUtil.showOkDialog(AlertDialogUtil.mapBuilder(context), R.string.dialog_title,
-						R.string.dialog_message);
+				AlertDialogUtil.showOkDialog(AlertDialogUtil.mapBuilder(context), mapDialog, message);
 			}
 		});
 
