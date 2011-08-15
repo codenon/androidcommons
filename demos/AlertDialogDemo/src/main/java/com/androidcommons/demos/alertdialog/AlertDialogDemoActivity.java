99ea85cca968db5c014999cdd5f96dba53e31cb4
denismigol <denismigol@46493231-44d8-eb8f-c521-964da5696728>
2011-08-15 00:24:05 +0000

    init AlertDialogDemo

---
+++ demos/AlertDialogDemo/src/main/java/com/androidcommons/demos/alertdialog/AlertDialogDemoActivity.java
@@ -0,0 +1,13 @@
+package com.androidcommons.demos.alertdialog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlertDialogDemoActivity extends Activity {
+
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
