f9ce0ba96d8ef3011f10b1d40dabab0a49d60687
denismigol <denismigol@46493231-44d8-eb8f-c521-964da5696728>
2011-08-27 01:39:49 +0000

    BitmapUtil.decodeStream

--- trunk/util/src/main/java/com/androidcommons/util/BitmapUtil.java
+++ trunk/util/src/main/java/com/androidcommons/util/BitmapUtil.java
@@ -5,11 +5,35 @@
  */
 package com.androidcommons.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.content.ContentResolver;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+
 /**
  * @author Denis Migol
  * 
  */
 public class BitmapUtil {
 	private BitmapUtil() {
+	}
+
+	public Bitmap decodeStream(final ContentResolver resolver, final Uri uri, final BitmapFactory.Options opts)
+			throws IOException {
+		InputStream is = resolver.openInputStream(uri);
+		try {
+			return BitmapFactory.decodeStream(is, null, opts);
+		} catch (final OutOfMemoryError e) {
+			System.gc();
+			is = resolver.openInputStream(uri);
+			final Bitmap bitmap = BitmapFactory.decodeStream(is, null, opts);
+			is.close();
+			return bitmap;
+		} finally {
+			is.close();
+		}
 	}
 }
