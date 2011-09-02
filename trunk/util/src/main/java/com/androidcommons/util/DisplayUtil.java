2dd8c7f57473e380a2be0a9468908d434607cea6
denismigol <denismigol@46493231-44d8-eb8f-c521-964da5696728>
2011-09-02 06:49:08 +0000

    DisplayUtil.getDisplayMetrics

--- trunk/util/src/main/java/com/androidcommons/util/DisplayUtil.java
+++ trunk/util/src/main/java/com/androidcommons/util/DisplayUtil.java
@@ -5,7 +5,9 @@
  */
 package com.androidcommons.util;
 
+import android.app.Activity;
 import android.content.Context;
+import android.util.DisplayMetrics;
 
 /**
  * @author Denis Migol
@@ -17,5 +19,15 @@ public final class DisplayUtil {
 
 	public static int dipToPx(final Context context, final int dip) {
 		return (int) (dip * context.getResources().getDisplayMetrics().density + 0.5f);
+	}
+
+	public static DisplayMetrics getDisplayMetrics(final Activity activity) {
+		final DisplayMetrics ret = new DisplayMetrics();
+		activity.getWindowManager().getDefaultDisplay().getMetrics(ret);
+		return ret;
+	}
+
+	public static DisplayMetrics getDisplayMetrics(final Context context) {
+		return context.getResources().getDisplayMetrics();
 	}
 }
