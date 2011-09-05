d1d7e10474e37afb65012a9be7abbf3c4a06e19f
denismigol <denismigol@46493231-44d8-eb8f-c521-964da5696728>
2011-09-05 21:52:13 +0000

    BitmapUtil: fix decodeStream (outRect was unused), computeSampleSize

--- util/src/main/java/com/androidcommons/util/BitmapUtil.java
+++ util/src/main/java/com/androidcommons/util/BitmapUtil.java
@@ -22,6 +22,8 @@ public final class BitmapUtil {
 	private BitmapUtil() {
 	}
 
+	private static final int UNCONSTRAINED = -1;
+
 	public static Bitmap decodeFile(final String pathName) {
 		try {
 			return BitmapFactory.decodeFile(pathName);
@@ -34,10 +36,10 @@ public final class BitmapUtil {
 	public static Bitmap decodeStream(final InputStream is, final Rect outPadding, final BitmapFactory.Options opts)
 			throws IOException {
 		try {
-			return BitmapFactory.decodeStream(is, null, opts);
+			return BitmapFactory.decodeStream(is, outPadding, opts);
 		} catch (final OutOfMemoryError e) {
 			System.gc();
-			final Bitmap bitmap = BitmapFactory.decodeStream(is, null, opts);
+			final Bitmap bitmap = BitmapFactory.decodeStream(is, outPadding, opts);
 			is.close();
 			return bitmap;
 		} finally {
@@ -59,5 +61,63 @@ public final class BitmapUtil {
 		} finally {
 			is.close();
 		}
+	}
+
+	private static int computeInitialSampleSize(final BitmapFactory.Options options, final int minSideLength,
+			final int maxNumOfPixels) {
+		final double w = options.outWidth;
+		final double h = options.outHeight;
+
+		final int lowerBound = (maxNumOfPixels == UNCONSTRAINED) ? 1 : (int) Math.ceil(Math
+				.sqrt(w * h / maxNumOfPixels));
+		final int upperBound = (minSideLength == UNCONSTRAINED) ? 128 : (int) Math.min(Math.floor(w / minSideLength),
+				Math.floor(h / minSideLength));
+
+		if (upperBound < lowerBound) {
+			// return the larger one when there is no overlapping zone.
+			return lowerBound;
+		}
+
+		if ((maxNumOfPixels == UNCONSTRAINED) && (minSideLength == UNCONSTRAINED)) {
+			return 1;
+		} else if (minSideLength == UNCONSTRAINED) {
+			return lowerBound;
+		} else {
+			return upperBound;
+		}
+	}
+
+	/*
+	 * Compute the sample size as a function of minSideLength and
+	 * maxNumOfPixels. minSideLength is used to specify that minimal width or
+	 * height of a bitmap. maxNumOfPixels is used to specify the maximal size in
+	 * pixels that is tolerable in terms of memory usage.
+	 * 
+	 * The function returns a sample size based on the constraints. Both size
+	 * and minSideLength can be passed in as UNCONSTRAINED, which indicates no
+	 * care of the corresponding constraint. The functions prefers returning a
+	 * sample size that generates a smaller bitmap, unless minSideLength =
+	 * UNCONSTRAINED.
+	 * 
+	 * Also, the function rounds up the sample size to a power of 2 or multiple
+	 * of 8 because BitmapFactory only honors sample size this way. For example,
+	 * BitmapFactory downsamples an image by 2 even though the request is 3. So
+	 * we round up the sample size to avoid OOM.
+	 */
+	public static int computeSampleSize(final BitmapFactory.Options options, final int minSideLength,
+			final int maxNumOfPixels) {
+		final int initialSize = computeInitialSampleSize(options, minSideLength, maxNumOfPixels);
+
+		int roundedSize;
+		if (initialSize <= 8) {
+			roundedSize = 1;
+			while (roundedSize < initialSize) {
+				roundedSize <<= 1;
+			}
+		} else {
+			roundedSize = (initialSize + 7) / 8 * 8;
+		}
+
+		return roundedSize;
 	}
 }
